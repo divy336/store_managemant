cc14a2403a0063a6c2696c7e5c3b8a48ecc5292b
nagar divy <nagardivya73@gmail.com>
2026-06-25 00:17:09 +0530

    final

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
 });
 
 api.interceptors.request.use(config => {
@@ -13,7 +13,6 @@ api.interceptors.request.use(config => {
 api.interceptors.response.use(
     response => response,
     error => {
-        // prettier-ignore
         if (error.response && error.response.status === 401) {
             localStorage.removeItem("token");
             localStorage.removeItem("admin");
